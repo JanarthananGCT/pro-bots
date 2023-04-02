2bb6e3d01ed3f3493fb766e64de7909d161dab85
Ideopedia <ideopedia00@gmail.com>
2023-04-02 14:20:30 +0530

    Ap21420

---
+++ components/dataTemp.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DataTemplate = (props) => {
+  return (
+    <div className="">
+      <div className=" w-[96vw] rounded-md min-h-[10vh] leading-8  bg-orange-500 flex px-4 items-center text-white font-normal text-[16px] md:text-xl lg:text-xl xl:text-xl ">
+        {props.que}
+      </div>
+      <div className=" w-[96vw] mt-5 rounded-md min-h-[20vh] leaading leading-8  flex px-4 items-center text-white font-normal text-[16px] md:text-xl lg:text-xl xl:text-xl ">
+        {JSON.parse(props.ans)}
+      </div>
+    </div>
+  );
+};
+
+export default DataTemplate;
