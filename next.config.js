f94ceeafb0cfade9eb0431ffe10ed6394208ffae
Ideopedia <ideopedia00@gmail.com>
2023-04-02 13:09:04 +0530

    second commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["oaidalleapiprodscus.blob.core.windows.net"],
-  },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  nextConfig,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      },
+    ],
+  },
+};
